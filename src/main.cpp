703fb5d86ff98776957267c3cae4926b478cab29
William Dubuc <william.dubuc01@gmail.com>
2025-10-16 09:20:20 -0400

    Add lib + initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
+#include <LibRobus.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+//-----------------------------
+// MAIN CODE.
+//-----------------------------
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    BoardInit();
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("TEST");
 }
