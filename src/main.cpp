65ba514790034b4c9852add5a7a71c9903e4fdff
William Dubuc <william.dubuc01@gmail.com>
2025-10-16 14:39:34 -0400

    Added PID, needs tweaking

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 #include "Detection.h"
+#include "WheelPID.h"
 
-#define IR_LEFT_PIN 2 // Vert
-#define IR_RIGHT_PIN 3 // Rouge
+#define IR_LEFT_PIN 39 // Vert
+#define IR_RIGHT_PIN 40 // Rouge
 
 //-----------------------------
 // MAIN CODE.
 //-----------------------------
 void setup() {
     BoardInit();
+    WheelPID::initPID();
     Serial.begin(115200);
     pinMode(IR_LEFT_PIN, INPUT);
     pinMode(IR_RIGHT_PIN, INPUT);
@@ -19,7 +21,7 @@ void loop() {
     Serial.println("TEST");
 }
 
-DETECTION::DetectionState readIR(){
+DETECTION::DetectionState getIRDetection(){
     bool isLeftOn = digitalRead(IR_LEFT_PIN);
     bool isRightOn = digitalRead(IR_LEFT_PIN);
     return DETECTION::getDetection(isLeftOn, isRightOn);
