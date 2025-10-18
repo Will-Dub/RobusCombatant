1f89b65fad39d2dbd3f1494f975ee001019a1c64
Fire-Cheese <sammycooly0.0@gmail.com>
2025-10-18 14:54:00 -0400

    wrote comment-code to have a basic structure of the task at hand. Will be developped once PID and movement functions will exist.
    Co-authored-by: laurent170 <laurent170@users.noreply.github.com>

---
+++ src/contourner_mur.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+
+//fonction qui permet de contourner le mur lorsque le capteur de couleur détecte le carré jaune.
+void contournerMur(){
+    //stop
+    //turn_right
+    //forward about 15cm
+    //turn_left
+    //forward some more
+    //turn_left
+    //forward until finds line
+    //gets back into position, and send it back to line following code
+
+
+}
