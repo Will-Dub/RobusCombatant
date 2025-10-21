a8f8b268836682a6047cc3a159871bb4ef1e5ec4
SimonPelletier1234 <simpelletier05@gmail.com>
2025-10-21 13:18:02 -0400

    Bug pour losange

---
+++ include/Losange.h
@@ -0,0 +1,4 @@
+#include "Movement.h"
+
+// Fait avancer le robot en forme de losange 
+void faireLosange();
---
+++ src/Losange.cpp
@@ -0,0 +1,26 @@
+#include "Losange.h"
+
+void faireLosange() {
+    float distance = 10.0; 
+
+    // Suit la ligne 
+
+    // tourne pour sortir de la ligne 
+    Movement::turnLeft(135);
+    Movement::moveForward(distance);
+
+    
+    Movement::turnLeft(45);
+    Movement::moveForward(distance);
+
+    
+    Movement::turnLeft(45);
+    Movement::moveForward(distance);
+
+    
+    Movement::turnLeft(45);
+    Movement::moveForward(distance);
+
+
+    Movement::stop();
+}
