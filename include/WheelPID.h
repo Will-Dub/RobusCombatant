1aa3be8d2d42e87bdb9fbb2086f3a7e6f8dcd66d
Fire-Cheese <sammycooly0.0@gmail.com>
2025-10-26 19:01:57 -0400

    updated PID.h code from morning branch

--- include/WheelPID.h
+++ include/WheelPID.h
@@ -1,5 +1,4 @@
-#ifndef WHEEL_PID_H
-#define WHEEL_PID_H
+#pragma once
 
 #include <LibRobus.h>
 #include <Arduino.h>
@@ -8,18 +7,19 @@ namespace WHEEL_PID{
     constexpr float PULSE_PER_TURN = 3200.0f;
     constexpr float POUCE_TO_CM = 2.54f;
     constexpr float ROUE_DIAMETRE = 3.0f;
-    const float kp = 0.01;
-    const float ki = 0.005;
-    const float kd = 0.001;
-    const float maxIntegral = 1000.0;
+    const float kp = 0.02f;
+    const float ki = 0.001f;
+    const float kd = 0.00005f;
+    const float maxIntegral = 300.0f;
 
-    void initPID();
+    void init();
     void setPIDDesiredPulse(int, int);
     void runPIDController();
+    void stopMotor();
     void reset();
     float getCoveredDistance();
     float getRightCoveredDistance();
     float getLeftCoveredDistance();
     void resetCoveredDistance();
+    float pulseToDistance(int pulse);
 }
-#endif
