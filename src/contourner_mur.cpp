7696032635509cae961f0ffb8ee10c356282ee85
Fire-Cheese <sammycooly0.0@gmail.com>
2025-10-26 15:53:47 -0400

    pre-made contourne_mur.cpp

--- src/contourner_mur.cpp
+++ src/contourner_mur.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#include "Movement.h"
 
 //fonction qui permet de contourner le mur lorsque le capteur de couleur détecte le carré jaune.
 void contournerMur(){
     //stop
-    //turn_right
-    //forward about 15cm
-    //turn_left
-    //forward some more
-    //turn_left
-    //forward until finds line
+    turnRight(90);
+    moveForward(15);
+    turnLeft(90);
+    moveForward(15);
+    turnLeft(90);
+    moveForward(15);
+    turnRight(90);
     //gets back into position, and send it back to line following code
 
 
